7b2f4a0fb23936c4b65ac740a8d66195bf3c2023
pythonDev161221 <adverbekreklamov@gmail.com>
2022-03-21 17:00:49 +0600

    add btn visibility some type change

--- source/webapp/static/js/index.js
+++ source/webapp/static/js/index.js
@@ -16,11 +16,24 @@ async function onLikeClick(event){
     let url = event.target.dataset.likesUrl;
     let response = await fetch(url);
     let responseBody = await response.json()
-    console.log(responseBody)
     let id = responseBody.article_id
-    console.log(id)
     div_like = document.getElementById(`${id}count`)
-    console.log(div_like)
     div_like.innerText = `likes: ${responseBody.like}`
+    document.getElementById(`${id}like_button`).style.visibility = "hidden"
+    document.getElementById(`${id}unlike_button`).style.visibility = "visible"
+    console.log(button_like)
+    button_like.style.display = 'toggle'
+}
+
+async function onUnLikeClick(event){
+    let url = event.target.dataset.likesUrl;
+    let response = await fetch(url);
+    let responseBody = await response.json()
+    let id = responseBody.article_id
+    div_like = document.getElementById(`${id}count`)
+    div_like.innerText = `likes: ${responseBody.like}`
+    document.getElementById(`${id}unlike_button`).style.visibility = 'hidden'
+    document.getElementById(`${id}like_button`).style.visibility = 'visible'
 
 }
+
