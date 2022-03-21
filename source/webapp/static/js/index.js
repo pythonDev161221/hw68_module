5b942a095e68fd17e45ead4ce867268a5cc46823
pythonDev161221 <adverbekreklamov@gmail.com>
2022-03-21 18:27:06 +0600

    add comment like and unlike works on js

--- source/webapp/static/js/index.js
+++ source/webapp/static/js/index.js
@@ -27,18 +27,35 @@ async function onUnLikeClick(event){
     let response = await fetch(url);
     let responseBody = await response.json();
     let id = responseBody.article_id;
-    div_like = document.getElementById(`${id}count`);
+    let div_like = document.getElementById(`${id}count`);
     div_like.innerText = `likes: ${responseBody.like}`;
     document.getElementById(`${id}unlike_button`).style.display = 'none';
     document.getElementById(`${id}like_button`).style.display = 'block';
 
 }
 
-async function commentLikeClick(event){
+async function commentLikeLikeClick(event){
     let url = event.target.dataset.likesUrl;
     console.log(url);
     let response = await fetch(url);
     let responseBody = await response.json();
     console.log(responseBody)
+    let id = responseBody.comment_id;
+    let div_comment_like = document.getElementById(`${id}likes`);
+    console.log(div_comment_like)
+    div_comment_like.innerText = `likes: ${responseBody.like}`;
 
+     document.getElementById(`${id}like_button`).style.display = 'none';
+    document.getElementById(`${id}unlike_button`).style.display = 'block';
+}
+async function commentUnLikeClick(event){
+    let url = event.target.dataset.likesUrl;
+    let response = await fetch(url);
+    let responseBody = await response.json();
+    let id = responseBody.comment_id;
+    let div_comment_like = document.getElementById(`${id}likes`);
+    div_comment_like.innerText = `likes: ${responseBody.like}`;
+
+     document.getElementById(`${id}unlike_button`).style.display = 'none';
+    document.getElementById(`${id}like_button`).style.display = 'block';
 }
